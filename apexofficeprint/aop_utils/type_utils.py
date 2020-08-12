62afedbc620dd7217c57544d18e567059980ce0d
Inias Peeters <iniasp@gmail.com>
2020-08-12 14:51:11 +0200

    Initial type_utils module

---
+++ apexofficeprint/aop_utils/type_utils.py
@@ -0,0 +1,17 @@
+import mimetypes
+
+
+def extension_to_mimetype(ext):
+    """
+    Map an extension to a mime type.
+    """
+    # append "a." because mimetypes can handle a format like "filename.extension", but not just "extension"
+    return mimetypes.guess_type(f"a.{ext}")[0]
+
+
+def mimetype_to_extension(mimetype):
+    """
+    Map a mime type to an extension.
+    """
+    # mimetypes returns a string in ".extension", so we strip off the first character
+    return mimetypes.guess_extension(mimetype)[1:]
