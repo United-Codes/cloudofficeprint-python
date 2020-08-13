d474dd4925039a85a29436b05a19465e849e1846
Inias Peeters <iniasp@gmail.com>
2020-08-13 18:29:35 +0200

    Expand PrintJob

--- apexofficeprint/printjob.py
+++ apexofficeprint/printjob.py
@@ -1,16 +1,49 @@
 import json
+from .aop_config import ServerConfig, OutputConfig
 
 STATIC_OPTS = {
     "tool": "python",
     "version": "18.2"
 }
 
+
 class PrintJob:
-    def __init__(self):
-        pass
+    def __init__(self,
+                 server_config: ServerConfig,
+                 output_config: OutputConfig):
+        self._server_config = server_config
+        self._output_config = output_config
 
     @property
     def json(self) -> str:
+        return json.dumps(self.as_dict)
+    
+    @property
+    def as_dict(self) -> dict:
         result = STATIC_OPTS
-        # add more stuff to result
-        return json.dumps(result)
+
+        if self.server_config.api_key is not None:
+            result["api_key"] = self.server_config.api_key
+
+        result["output"] = self.output_config.as_dict
+
+        # add more stuff to result here
+
+        return result
+
+    # TODO: docstrings
+    @property
+    def server_config(self):
+        return self._server_config
+
+    @server_config.setter
+    def server_config(self, value: ServerConfig):
+        self._server_config = value
+
+    @property
+    def output_config(self):
+        return self._output_config
+
+    @output_config.setter
+    def output_config(self, value: OutputConfig):
+        self._output_config = value
