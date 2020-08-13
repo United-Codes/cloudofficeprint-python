8f2f01430f89d9f2820624755c7212288cf1c6cd
Inias Peeters <iniasp@gmail.com>
2020-08-13 18:29:47 +0200

    Update __init__.py files

--- apexofficeprint/__init__.py
+++ apexofficeprint/__init__.py
@@ -1,3 +1,4 @@
 from .resource import Resource
 from .aop_error import AOPError
-from .aop_config import ServerConfig
+from .aop_config import ServerConfig, OutputConfig, PDFOptions
+from .printjob import PrintJob
--- apexofficeprint/aop_config/__init__.py
+++ apexofficeprint/aop_config/__init__.py
@@ -1,1 +1,4 @@
 from .server_config import ServerConfig
+from .output_config import OutputConfig
+from .cloud_access_token import CloudAccessToken
+from .pdf_options import PDFOptions
