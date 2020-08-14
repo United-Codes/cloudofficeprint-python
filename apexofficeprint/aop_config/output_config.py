8070ede5b0e10bd8f2c2d8a579e7f357912ae629
Inias Peeters <iniasp@gmail.com>
2020-08-14 09:50:04 +0200

    Docstrings

--- apexofficeprint/aop_config/output_config.py
+++ apexofficeprint/aop_config/output_config.py
@@ -14,13 +14,13 @@ class OutputConfig:
                  encoding: str = "raw",
                  converter: str = "libreoffice",
                  cloud_access_token: CloudAccessToken = None,
-                 server_location: str = None,
+                 server_directory: str = None,
                  pdf_options: PDFOptions = None):
         self._filetype = filetype
         self._encoding = encoding
         self._converter = converter
         self._cloud_access_token = cloud_access_token
-        self._server_location = server_location
+        self._server_directory = server_directory
         self._pdf_options = pdf_options
 
     @property
@@ -49,56 +49,126 @@ class OutputConfig:
             result["output_type"] = self._filetype
         if self._cloud_access_token is not None:
             result.update(self._cloud_access_token.as_dict)
-        if self._server_location is not None:
-            result["output_directory"] = self._server_location
+        if self._server_directory is not None:
+            result["output_directory"] = self._server_directory
         if self._pdf_options is not None:
             result.update(self._pdf_options)
 
-    # TODO: docstrings
     @property
     def filetype(self) -> str:
+        """The file type (as extension) to use for the output.
+
+        Returns:
+            str: filetype
+        """
         return self._filetype
 
     @filetype.setter
     def filetype(self, value: str):
+        """Set filetype
+
+        Args:
+            value (str): filetype
+        """
         self._filetype = value
 
     @property
     def encoding(self) -> str:
+        """The encoding to use, either "raw" or "base64".
+
+        Returns:
+            str: encoding
+        """
         return self._encoding
 
     @encoding.setter
     def encoding(self, value: str):
+        """Set encoding.
+
+        Args:
+            value (str): encoding
+        """
+        if not (value == "raw" or value == "base64"):
+            raise ValueError(f'Encoding must be either "raw" or "base64", was "{value}".')
         self._encoding = value
 
     @property
     def converter(self) -> str:
+        """The pdf converter to use.
+
+        Can be "libreoffice", "officetopdf" or any custom defined converter.
+        Custom converters are configurated in the AOP server's `aop_config.json` file.
+
+        Returns:
+            str: PDF converter name
+        """
         return self._converter
 
     @converter.setter
     def converter(self, value: str):
+        """Set converter
+
+        Args:
+            value (str): PDF converter name
+        """
         self._converter = value
 
     @property
     def cloud_access_token(self) -> CloudAccessToken:
+        """Access token used to access various cloud services for output storage.
+
+        It should be an instance of apexofficeprint.CloudAccessToken.
+
+        Returns:
+            CloudAccessToken: access token
+        """
         return self._cloud_access_token
 
     @cloud_access_token.setter
     def cloud_access_token(self, value: CloudAccessToken):
+        """Set cloud_access_token
+
+        Args:
+            value (CloudAccessToken): cloud_access_token
+        """
         self._cloud_access_token = value
 
     @property
-    def server_location(self) -> str:
-        return self._server_location
+    def server_directory(self) -> str:
+        """Base directory to save output files into if the server allows save on disk.
 
-    @server_location.setter
-    def server_location(self, value: str):
-        self._server_location = value
+        The specific output path for each file is appended to the base path.
+
+        Returns:
+            str: path to the base directory on the server
+        """
+        return self._server_directory
+
+    @server_directory.setter
+    def server_directory(self, value: str):
+        """Set server_directory
+
+        Args:
+            value (str): server_directory
+        """
+        self._server_directory = value
 
     @property
     def pdf_options(self) -> PDFOptions:
+        """Optional PDF options.
+
+        Should be an instance of apexofficeprint.PDFOptions
+
+        Returns:
+            PDFOptions: PDF options
+        """
         return self._pdf_options
 
     @pdf_options.setter
     def pdf_options(self, value: PDFOptions):
+        """Set pdf_options
+
+        Args:
+            value (PDFOptions): pdf_options
+        """
         self._pdf_options = value
--- apexofficeprint/printjob.py
+++ apexofficeprint/printjob.py
@@ -6,6 +6,8 @@ STATIC_OPTS = {
     "version": "18.2"
 }
 
+# TODO: somewhere, this class should check whether its templates are compatible and whether they are in turn compatible with the output file type
+
 
 class PrintJob:
     def __init__(self,
@@ -16,10 +18,23 @@ class PrintJob:
 
     @property
     def json(self) -> str:
+        """json equivalent of the dict representation of this print job.
+
+        Returns:
+            str: json representation of this print job
+        """
         return json.dumps(self.as_dict)
-    
+
     @property
     def as_dict(self) -> dict:
+        """dict representation of this print job.
+
+        This representation is isomorphic to the json representation
+        to send to the server.
+
+        Returns:
+            dict: dict representation of this print job
+        """
         result = STATIC_OPTS
 
         if self.server_config.api_key is not None:
@@ -31,19 +46,40 @@ class PrintJob:
 
         return result
 
-    # TODO: docstrings
     @property
-    def server_config(self):
+    def server_config(self) -> ServerConfig:
+        """Server configuration to be used for this print job.
+
+        Should be an instance of apexofficeprint.ServerConfig
+
+        Returns:
+            ServerConfig: server configuration
+        """
         return self._server_config
 
     @server_config.setter
     def server_config(self, value: ServerConfig):
+        """Set server_config
+
+        Args:
+            value (ServerConfig): server_config
+        """
         self._server_config = value
 
     @property
-    def output_config(self):
+    def output_config(self) -> OutputConfig:
+        """Output configuration to be used for this print job.
+
+        Returns:
+            OutputConfig: output configuration
+        """
         return self._output_config
 
     @output_config.setter
     def output_config(self, value: OutputConfig):
+        """Set output_config
+
+        Args:
+            value (OutputConfig): output_config
+        """
         self._output_config = value
