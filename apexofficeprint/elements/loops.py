54db14cc95f3f9bf3099f38a7b0fb160885c00ee
Inias Peeters <iniasp@gmail.com>
2020-08-21 16:22:44 +0200

    Differentiate forEachSheet
    Also allow dict with sheet name keys as constructor input, making it different from forEachSlide

--- apexofficeprint/elements/loops.py
+++ apexofficeprint/elements/loops.py
@@ -1,10 +1,13 @@
-from .elements import Element
-from typing import Iterable, FrozenSet
+from .elements import Element, Object, Property
+from typing import Iterable, FrozenSet, Union, Mapping
+
 
 class ForEach(Element):
     def __init__(self, name: str, content: Iterable[Element]):
         super().__init__(name)
         self._content = list(content)
+        # if self._tags should be overwritten in a subclass of this one,
+        # remember to do so after calling super().__init__
         self._tags = {
             "{#" + name + "}",
             "{/" + name + "}"
@@ -50,6 +53,29 @@ class ForEachSlide(ForEach):
         }
 
 
+class ForEachSheet(ForEach):
+    def __init__(self, name: str, content: Union[Iterable[Element], Mapping[str, Element]]):
+        # when content is a mapping, it means "sheet name": content for that sheet
+        # when it's just an iterable, don't add sheet names (or they can do the Property manually)
+        if isinstance(content, Mapping):
+            new_content = []
+            for sheetname, sheetcontent in content:
+                # we need to add the additional sheet_name property,
+                # so we should convert the Element to to an Object if needed
+                if not isinstance(sheetcontent, Object):
+                    sheetcontent = Object.element_to_object(sheetcontent)
+                # adding the new property containing sheet_name
+                sheetcontent.add(Property("sheet_name", sheetname))
+                new_content.append(sheetcontent)
+            content = new_content
+        # at this point, content is always Iterable[Element]
+
+        super().__init__(name, content)
+        self._tags = {
+            "{!" + name + "}"
+        }
+
+
 class ForEachInline(ForEach):
     def __init__(self, name: str, content: Iterable[Element]):
         super().__init__(name, content)
@@ -70,5 +96,4 @@ class ForEachTableRow(ForEach):
 
 # These are the same, but they may not be forever
 # and combining them into one class breaks consistency
-ForEachSheet = ForEachSlide  # TODO: sheet name
 ForEachHorizontal = ForEachInline
