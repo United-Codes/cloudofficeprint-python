d986bdc777101aae00e5e832d8dc7eca569a2382
Inias Peeters <iniasp@gmail.com>
2020-11-15 15:00:29 +0100

    Declare license classifier (used for searching by license if this should go on PyPI), repo is still missing the license itself

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        #"License :: OSI Approved :: MIT License", # TODO
+        "LICENSE :: OSI APPROVED :: GNU GENERAL PUBLIC LICENSE V3 (GPLV3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
