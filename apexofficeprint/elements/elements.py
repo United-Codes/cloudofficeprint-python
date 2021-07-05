59ca7dea2158d6d392a75b0468ae7a0c9852e6e2
Jens Putzeys <jens.putzeys@student.kuleuven.be>
2021-07-05 17:16:02 +0200

    split class Code in elements.py into subclasses BarCode and QRCode and split QRCode into more subclasses for each type of QR-code: fix issue #28

--- apexofficeprint/elements/elements.py
+++ apexofficeprint/elements/elements.py
@@ -340,32 +340,13 @@ class D3Code(Property):
 
 
 class Code(Element):
-    """# TODO: refer to this link for type argument (jumps to the right docs section)
-    http://www.apexofficeprint.com/docs/#barcode-qrcode-tags
-    """
-
-    def __init__(self,
-                 name: str,
-                 data: str,
-                 height: int = None,
-                 width: int = None,
-                 link_url: str = None,
-                 rotation: int = None,
-                 background_color: str = None,
-                 padding_width: int = None,
-                 padding_height: int = None,
-                 qr_error_correction_level: str = None):
+    """Superclass for QR-codes and barcodes"""
+    def __init__(self, name: str, data: str, type: str):
         super().__init__(name)
         self.data: str = data
-        self.height: int = height
-        self.width: int = width
-        self.link_url: str = link_url
-        self.rotation: int = rotation
-        self.background_color: str = background_color
-        self.padding_width: int = padding_width
-        self.padding_height: int = padding_height
-        self.qr_error_correction_level: str = qr_error_correction_level
-
+        self.type: str = type
+        """For the different types of QR-codes and barcodes, we refer to the AOP documentation: http://www.apexofficeprint.com/docs/#barcode-qrcode-tags"""
+    
     @property
     def available_tags(self) -> FrozenSet[str]:
         return frozenset({"{|" + self.name + "}"})
@@ -374,22 +355,8 @@ class Code(Element):
     def _dict_suffixes(self):
         result = {}
 
-        if self.height:
-            result["_height"] = self.height
-        if self.width:
-            result["_width"] = self.width
-        if self.link_url:
-            result["_url"] = self.link_url
-        if self.rotation:
-            result["_rotation"] = self.rotation
-        if self.background_color:
-            result["_background_color"] = self.background_color
-        if self.padding_width:
-            result["_padding_width"] = self.padding_width
-        if self.padding_height:
-            result["_padding_height"] = self.padding_height
-        if self.qr_error_correction_level:
-            result["_errorcorrectlevel"] = self.qr_error_correction_level
+        if self.type is not None:
+            result["_type"] = self.type
 
         return result
 
@@ -401,6 +368,268 @@ class Code(Element):
 
         for suffix, value in self._dict_suffixes.items():
             result[self.name + suffix] = value
+
+        return result
+    
+class BarCode(Code):
+    """This class is a subclass of Code and is used to generate a barcode element"""
+    def __init__(
+            self,
+            name: str,
+            data: str,
+            type: str,
+            height: int = None,
+            width: int = None,
+            errorcorrectlevel: str = None,
+            url: str = None,
+            rotation: int = None,
+            background_color: str = None,
+            padding_width: int = None,
+            padding_height: int = None,
+            extra_options: str = None
+        ):
+        super().__init__(name, data, type)
+        self.height: int = height
+        """ This field contains the height for the generated image. Default is 200 for QR, 50 for the rest."""
+        self.width: int = width
+        """This field contains the width for the generated image. Default is 200."""
+        self.errorcorrectlevel: str = errorcorrectlevel
+        """This field contains the level of which the QR code should be recoverable. The options are:
+            "L" (up to 7% damage)
+            "M" (up to 15% damage)
+            "Q" (up to 25% damage)
+            "H" (up to 30% damage)
+        """
+        self.url: str = url
+        """The URL to hyperlink to when the barcode/qrcode is clicked"""
+        self.rotation: int = rotation
+        """The rotation angle of the barcode/qrcode (in degrees, counterclockwise)"""
+        self.background_color: str = background_color
+        """The background color for the barcode/qrcode. default: white/ffffff.
+        You can provide hex value; html named colors like red, white, purple; rgb(255, 0, 0) ; or any other css supported format."""
+        self.padding_width: int = padding_width
+        """The padding of the inserted qrcode/barcode. default 10. In pixels"""
+        self.padding_height: int = padding_height
+        """The padding of the inserted qrcode/barcode. default 10. In pixels"""
+        self.extra_options: str = extra_options
+        """If you want to include extra options like including barcode text on the bottom. 
+        The options should be space separated and should be followed by a "=" and their value.
+        E.g.: "includetext guardwhitespace guardwidth=3 guardheight=3".
+        Please visit: https://github.com/bwipp/postscriptbarcode/wiki/Symbologies-Reference for all option availability."""
+    
+    @property
+    def _dict_suffixes(self):
+        result = super()._dict_suffixes
+
+        if self.height is not None:
+            result['_height'] = self.height
+        if self.width is not None:
+            result['_width'] = self.width
+        if self.errorcorrectlevel is not None:
+            result['_errorcorrectlevel'] = self.errorcorrectlevel
+        if self.url is not None:
+            result['_url'] = self.url
+        if self.rotation is not None:
+            result['_rotation'] = self.rotation
+        if self.background_color is not None:
+            result['_background_color'] = self.background_color
+        if self.padding_width is not None:
+            result['_padding_width'] = self.padding_width
+        if self.padding_height is not None:
+            result['_padding_height'] = self.padding_height
+        if self.extra_options is not None:
+            result['_extra_options'] = self.extra_options
+
+        return result
+
+
+class QRCode(Code):
+    """This class is a subclass of Code and serves as a superclass for the different types of QR-codes"""
+    # TODO: check which fields are optional for the different QRCode types and change _dict_suffixes() function if needed (if ... is not None:)
+    # TODO: create setters (or constructor) for QR-code options (see documentation)
+    def __init__(self, name: str, data: str, type: str):
+        super().__init__(name, data, type)
+    
+    @property
+    def _dict_suffixes(self):
+        result = super()._dict_suffixes
+
+        return result
+
+
+class WiFiQRCode(QRCode):
+    """This class is a subclass of QRCode and is used to generate a WiFi QR-code element"""
+    def __init__(
+        self,
+        name: str,
+        ssid: str,
+        wifi_password: str,
+        wifi_encryption: str,
+        wifi_hidden: bool
+    ):
+        super().__init__(name, ssid, 'qr_wifi')
+        self.wifi_password: str = wifi_password
+        self.wifi_encryption: str = wifi_encryption
+        self.wifi_hidden: bool = wifi_hidden
+    
+    @property
+    def _dict_suffixes(self):
+        result = super()._dict_suffixes
+
+        result['_wifi_password'] = self.wifi_password
+        result['_wifi_encryption'] = self.wifi_encryption
+        result['_wifi_hidden'] = self.wifi_hidden
+
+        return result
+    
+class TelephoneNumberQRCode(QRCode):
+    """This class is a subclass of QRCode and is used to generate a telephone number QR-code element"""
+    def __init__(self, name: str, number: str):
+        super().__init__(name, number, 'qr_telephone')
+
+class EmailQRCode(QRCode):
+    """This class is a subclass of QRCode and is used to generate an email QR-code element"""
+    def __init__(
+        self,
+        name: str,
+        receiver: str,
+        cc: str,
+        bcc: str,
+        subject: str,
+        body: str
+    ):
+        super().__init__(name, receiver, 'qr_email')
+        self.cc: str = cc
+        self.bcc: str = bcc
+        self.subject: str = subject
+        self.body: str = body
+    
+    @property
+    def _dict_suffixes(self):
+        result = super()._dict_suffixes
+
+        result['_email_cc'] = self.cc
+        result['_email_bcc'] = self.bcc
+        result['_email_subject'] = self.subject
+        result['_email_body'] = self.body
+
+        return result
+
+
+class SMSQRCode(QRCode):
+    """This class is a subclass of QRCode and is used to generate an SMS QR-code element"""
+    def __init__(self, name: str, receiver: str, sms_body: str):
+        super().__init__(name, receiver, 'qr_sms')
+        self.sms_body: str = sms_body
+
+    @property
+    def _dict_suffixes(self):
+        result = super()._dict_suffixes
+
+        result['_sms_body'] = self.sms_body
+
+        return result
+
+
+class URLQRCode(QRCode):
+    """This class is a subclass of QRCode and is used to generate a URL QR-code element"""
+    def __init__(self, name: str, url: str):
+        super().__init__(name, url, 'qr_url')
+
+
+class VCardQRCode(QRCode):
+    """This class is a subclass of QRCode and is used to generate a vCard QR-code element"""
+    def __init__(self, name: str, first_name: str, last_name: str, email: str, website: str):
+        super().__init__(name, first_name, 'qr_vcard')
+        self.last_name: str = last_name
+        self.email: str = email
+        self.website: str = website
+
+    @property
+    def _dict_suffixes(self):
+        result = super()._dict_suffixes
+
+        result['_vcard_last_name'] = self.last_name
+        result['_vcard_email'] = self.email
+        result['_vcard_website'] = self.website
+
+        return result
+
+
+class MeCard(QRCode):
+    """This class is a subclass of QRCode and is used to generate a MeCard QR-code element"""
+    def __init__(
+        self,
+        name: str,
+        first_name: str,
+        last_name: str,
+        nickname: str,
+        email: str,
+        contact_primary: str,
+        contact_secondary: str,
+        contact_tertiary: str,
+        website: str,
+        birthday: str,
+        notes: str
+    ):
+        super().__init__(name, first_name, 'qr_me_card')
+        self.last_name: str = last_name
+        self.nickname: str = nickname
+        self.email: str = email
+        self.contact_primary: str = contact_primary
+        self.contact_secondary: str = contact_secondary
+        self.contact_tertiary: str = contact_tertiary
+        self.website: str = website
+        self.birthday: str = birthday
+        self.notes: str = notes
+    
+    @property
+    def _dict_suffixes(self):
+        result = super()._dict_suffixes
+
+        result['_me_card_last_name'] = self.last_name
+        result['_me_card_nickname'] = self.nickname
+        result['_me_card_email'] = self.email
+        result['_me_card_contact_primary'] = self.contact_primary
+        result['_me_card_contact_secondary'] = self.contact_secondary
+        result['_me_card_contact_tertiary'] = self.contact_tertiary
+        result['_me_card_website'] = self.website
+        result['_me_card_birthday'] = self.birthday
+        result['_me_card_notes'] = self.notes
+
+        return result
+
+
+class GeolocationQRCode(QRCode):
+    """This class is a subclass of QRCode and is used to generate a geolocation QR-code element"""
+    def __init__(self, name: str, latitude: str, longitude: str, altitude: str):
+        super().__init__(name, latitude, 'qr_geolocation')
+        self.longitude: str = longitude
+        self.altitude: str = altitude
+
+    @property
+    def _dict_suffixes(self):
+        result = super()._dict_suffixes
+
+        result['_geolocation_longitude'] = self.longitude
+        result['_geolocation_altitude'] = self.altitude
+
+        return result
+
+
+class EventQRCode(QRCode):
+    """This class is a subclass of QRCode and is used to generate an event QR-code element"""
+    def __init__(self, name: str, summary: str, startdate: str, enddate: str):
+        super().__init__(name, summary, 'qr_event')
+        self.startdate: str = startdate
+        self.enddate: str = enddate
+
+    @property
+    def _dict_suffixes(self):
+        result = super()._dict_suffixes
+
+        result['_event_startdate'] = self.startdate
+        result['_event_enddate'] = self.enddate
 
         return result
 
