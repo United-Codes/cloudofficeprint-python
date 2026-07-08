a8250368dbcf0fd5ccb4d44b30ece4fafc8c2c6b
prabinbessie <bhandari16963@icloud.com>
2026-07-08 15:57:20 +0545

    update path info

--- BeginerGuide/UsingPdfTags/usingPdfTags.py
+++ BeginerGuide/UsingPdfTags/usingPdfTags.py
@@ -1,6 +1,5 @@
-import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+sys.path.insert(0, "PATH_TO_COP_DIR")
 import cloudofficeprint as cop
 
 collection = cop.elements.ElementCollection()
